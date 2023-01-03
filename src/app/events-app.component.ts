534e29a01a03de17817906f4c076f1636232ee6d
ArwaNashaat <arwanashaat@gmail.com>
2023-01-03 21:32:02 +0200

    add bootstrap and images

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app-root',
-  template: '<h1>Hello World</h1>',
+  template: `<h1>Hello World</h1>
+    <img src="/assets/images/basic-shield.png" />`,
 })
 export class EventsAppComponent {
   title = 'Events';
